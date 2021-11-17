4c03fab189feddeedd4d718b4bf7c4d822a55197
Matte22 <mferreira1264@gmail.com>
2021-11-17 11:46:09 -0500

    trying to fix sourse

--- Csc212sparsematrices/Source.cpp
+++ Csc212sparsematrices/Source.cpp
@@ -4,6 +4,10 @@
 #include <vector>
 #include<fstream>
 #include<sstream>
+#include <iostream>
+#include <vector>
+#include<fstream>
+#include<sstream>
 
 
 //function declarations
@@ -17,6 +21,21 @@ void PrintMatrix(std::vector<std::vector<int>> matrix);
 	PrintMatrix(matrix1);
 	std::cout << std:: endl;
 	PrintMatrix(matrix2);
+	int main(int argc, char* argv[]) {
+		//arguments ./prog matrix1.txt matrix2.txt
+		std::string file1 = argv[1];
+		std::string file2 = argv[2];
+		int i = 1 + 2;
+		int j = 5;
+
+
+		//testing readFile function with matrix txt files
+		std::vector<std::vector<int>> matrix1 = ReadFile(file1);
+		std::vector<std::vector<int>> matrix2 = ReadFile(file2);
+
+		PrintMatrix(matrix1);
+		std::cout << std::endl;
+		PrintMatrix(matrix2);
 
 
 	return 0;
@@ -26,26 +45,34 @@ void PrintMatrix(std::vector<std::vector<int>> matrix);
 void PrintMatrix(std::vector<std::vector<int>> matrix){
 	for(int i=0; i< matrix.size(); i++){
 		for(int j=0; j< matrix[0].size(); j++){
+void PrintMatrix(std::vector<std::vector<int>> matrix) {
+	for (int i = 0; i < matrix.size(); i++) {
+		for (int j = 0; j < matrix[0].size(); j++) {
 			std::cout << matrix[i][j] << " ";
 		}
 		std::cout << std::endl;
 	}
 
 }
+
 //This function reads text files and returns 2d vector matrices
 std::vector<std::vector<int>> ReadFile(std::string fileName){
+std::vector<std::vector<int>> ReadFile(std::string fileName) {
 	std::ifstream inFile(fileName);
 	std::vector<std::vector<int>> matrix;
 	std::string line;
 	while(getline(inFile, line)){
+	while (getline(inFile, line)) {
 		std::vector<int> row;
 		std::istringstream sstream(line);
 		int num;
 		while(sstream>>num){
+		while (sstream >> num) {
 			row.push_back(num);
 		}
 		matrix.push_back(row);
 	}
 	inFile.close();
 	return matrix;
+}
 }
