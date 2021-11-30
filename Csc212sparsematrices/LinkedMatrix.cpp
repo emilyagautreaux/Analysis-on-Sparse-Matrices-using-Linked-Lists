193e7f4d40f08d62ab7e038252d8a1cee387d577
Amoy Scott <78177247+ascott20@users.noreply.github.com>
2021-11-30 12:01:20 -0500

    Revert "just to get my name on the contribution list"
    This reverts commit 145cebc18915169f3845e242f56bfa9202f330c9.

--- Csc212sparsematrices/LinkedMatrix.cpp
+++ Csc212sparsematrices/LinkedMatrix.cpp
@@ -181,9 +181,7 @@ void LinkedMatrix::print(Node* node, std::string type) { // the parameter is the
 	}
 	std::cout << "nullptr";
 }
-<<<<<<< HEAD
-//inverse function here
-=======
+
 
 //Convert linked list to 2D vector 
 std::vector<std::vector<int>> LinkedMatrix::Linked2Vector(int m1rows, int m1cols, int m2rows, int m2cols, std::string type){
@@ -304,3 +302,6 @@ std::vector<std::vector<int>> LinkedMatrix::Linked2Vector(int m1rows, int m1cols
 	}
 }
 >>>>>>> e57e845271c6817c8602f7fd17d6b633178576f7
+=======
+
+>>>>>>> parent of 145cebc (just to get my name on the contribution list)
